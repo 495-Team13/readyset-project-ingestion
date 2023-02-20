46f7cb96c6a06de350cafd192d94859c6380e035
oyrussell <oyrussell@crimson.ua.edu>
2023-02-20 00:10:37 -0600

    Adding Comments for the CRUD methods

--- API/app.py
+++ API/app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/projects', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/api/projects', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def projects():
     #Will return a json object when needing to return project data
     if request.method == 'GET':
@@ -11,25 +11,31 @@ def projects():
         #client = MongoClient("<MongoDB_URL>")
         exit
     if request.method == "POST":
+        #create a new project in the mongoDB database
         exit
     if request.method == "PUT":
+        #update an existing project in the mongoDB projects database
         exit
     if request.method == "DELETE":
+        #delete an existing project in the mongoDB projects database
         exit
 
     return
 
-@app.route('/templates', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/api/templates', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def templates():
     #Will return a json object when needing to return template data
     if request.method == 'GET':
+        #Fetch template data from the mongoDB database (returns as json)
         exit
     if request.method == "POST":
+        #create a new template in the mongoDB database
         exit
     if request.method == "PUT":
+        #update an existing template in the mongoDB database (edit)
         exit
     if request.method == "DELETE":
+        #delete an existing template from the mongoDB database
         exit
-        
     return
     
