97ec0cf54792627a5f12b4b7f97867d079c94144
Josh <jclacount@crimson.ua.edu>
2023-02-26 23:39:05 +0000

    update and delete project

--- db.py
+++ db.py
@@ -11,6 +11,9 @@ class DBClient:
     def __init__(self, host, port, user, password, auth_source):
         uri = 'mongodb://%s:%s@%s:%d/?authSource=%s' % (quote_plus(user), quote_plus(password), host, port, quote_plus(auth_source))
         self.client = MongoClient(uri)
+        self.projects = self.client[PI_DB][PROJECTS_COL]
+        self.products = self.client[PI_DB][PRODUCTS_COL]
+        self.templates = self.client[PI_DB][TEMPLATES_COL]
 
     def check_connection(self):
         try:
@@ -23,9 +26,7 @@ class DBClient:
         return False
 
     def add_project(self):
-        projects = self.client[PI_DB][PROJECTS_COL]
-
-        untitled_projects = list(projects.find( { 'name': { '$regex': r'^untitled\d+$', '$options': 'i' } } ))
+        untitled_projects = list(self.projects.find( { 'name': { '$regex': r'^untitled\d+$', '$options': 'i' } } ))
         max_i = max([int(re.search(r'\d+$', proj['name']).group()) for proj in untitled_projects]) if len(untitled_projects) else 0
         name = f'Untitled{max_i+1}'
         project = {
@@ -33,7 +34,13 @@ class DBClient:
             'products': []
         }
         
-        return projects.insert_one(project)
+        return name, self.projects.insert_one(project)
+
+    def update_project(self, name, updated_fields):
+        return self.projects.update_one({'name': name}, {'$set': updated_fields})
+
+    def delete_project(self, name):
+        return self.projects.delete_one({'name': name})
 
 if __name__ == '__main__':
     host = '192.168.1.28'
@@ -42,4 +49,3 @@ if __name__ == '__main__':
     password = 'password'
     auth_source = 'admin'
     client = DBClient(host, port, user, password, auth_source)
-    client.add_project()
