96779ff1e65fcec5fa2c1af2ff362fddf54c4b99
Owen Russell <47670022+oyrussell@users.noreply.github.com>
2023-02-27 14:28:03 -0600

    Added Product and Template API endpoint frames

--- API/authTest.py
+++ API/authTest.py
@@ -1,4 +1,4 @@
-#Just testing Authorization tokens for Flask
+# Framing the API
 
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
@@ -43,7 +43,7 @@ def has_permission(permission):
     return wrapper
 
 
-#Projects API Endpoints
+#Projects API Endpoints     #############################
 
 # Protected API endpoint
 @app.route('/api/projects/get', methods=['GET'])
@@ -59,6 +59,7 @@ def add_new_project():
     data = [{'name' : "untitled_01", 'products' : NULL}]
     return jsonify(data=data), 201  #201 = New Resource Created
 
+# Edit existing Project Protected API endpoint
 @app.route('api/projects/edit', methods = ['PUT'])
 @has_permission('write')
 def edit_project(project_name):
@@ -66,12 +67,79 @@ def edit_project(project_name):
     #
     return 400  #Unfinished
 
+# Delete existing Project Protected API endpoint
 @app.route('api/projects/delete', methods = ['DELETE'])
 @has_permission('write')
 def delete_project(project_name):
     #Call Delete Project Function in db.py
     #
     return 400 #Unfinished
+
+
+#Products API Endpoints     #############################
+
+# Retrieve product Protected API endpoint
+@app.route('/api/products/get', methods=['GET'])
+@has_permission('read')
+def get_product_data(product_name):
+    data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
+    return jsonify(data=data), 200 #200 = Resource retrieved successfully 
+
+# Create new Product Protected API endpoint
+@app.route('api/products/add', methods = ['POST'])
+@has_permission('write')
+def add_new_product():
+    data = [{'name' : "untitled_01", 'products' : NULL}]
+    return jsonify(data=data), 201  #201 = New Resource Created
+
+# Edit existing Product Protected API endpoint
+@app.route('api/products/edit', methods = ['PUT'])
+@has_permission('write')
+def edit_product(product_name):
+    #Retrieve Product from Mongodb
+    #
+    return 400  #Unfinished
+
+# Delete existing Product Protexted API endpoint
+@app.route('api/products/delete', methods = ['DELETE'])
+@has_permission('write')
+def delete_product(product_name):
+    #Call Delete Product Function in db.py
+    #
+    return 400 #Unfinished
+
+#Templates API Endpoints     #############################
+
+# Retrieve Template Protected API endpoint
+@app.route('/api/templates/get', methods=['GET'])
+@has_permission('read')
+def get_template_data(template_name):
+    data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
+    return jsonify(data=data), 200 #200 = Resource retrieved successfully 
+
+# Create new Template Protected API endpoint
+@app.route('api/templates/add', methods = ['POST'])
+@has_permission('write')
+def add_new_template():
+    data = [{'name' : "untitled_01", 'products' : NULL}]
+    return jsonify(data=data), 201  #201 = New Resource Created
+
+# Edit existing Template Protected API endpoint
+@app.route('api/templates/edit', methods = ['PUT'])
+@has_permission('write')
+def edit_template(template_name):
+    #Retrieve Product from Mongodb
+    #
+    return 400  #Unfinished
+
+# Delete existing Template Protexted API endpoint
+@app.route('api/templates/delete', methods = ['DELETE'])
+@has_permission('write')
+def delete_template(template_name):
+    #Call Delete Product Function in db.py
+    #
+    return 400 #Unfinished
+
 
 
 # Login endpoint
