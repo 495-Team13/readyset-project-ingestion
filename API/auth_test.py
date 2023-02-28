b8a33aca7a9688d11382989423ab1a075772c6e9
oyrussell <oyrussell@crimson.ua.edu>
2023-02-28 11:34:13 -0600

    Removed Broken API Build, Added UnitTests

--- API/auth_test.py
+++ API/auth_test.py
@@ -10,9 +10,9 @@ class FlaskTest(unittest.TestCase):
     #Checking for Response 200
     def test_index(self):
         tester = app.test_client(self)
-        response = tester.get("/api/foo/")
+        response = tester.get("/api/projects/get/")
         statuscode = response.status_code
-        self.assertEquals(statuscode, 200)
+        self.assertEquals(statuscode, 401)
 
 if __name__ == "__main__":
     unittest.main() 
