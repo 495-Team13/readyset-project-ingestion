fb9363b2dde9b5bec7530fc1f7b55b90cdb86032
oyrussell <oyrussell@crimson.ua.edu>
2023-02-28 12:47:31 -0600

    Protected All Endpoints

--- API/app.py
+++ API/app.py
@@ -59,7 +59,7 @@ fetch('/api/login/', {
 
 # Protected API endpoint
 @app.route('/api/projects/get/<project_name>', methods=['GET'])
-@jwt_required()          #Will be how I protect the endpoints, but I  cant send a correct login request yet (Working on fix)
+@jwt_required()
 def get_data(project_name):
     data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
     return jsonify(data=data), 200 #200 = Resource retrieved successfully 
@@ -83,6 +83,7 @@ fetch('/api/projects/get/' + project_name, {
 
 # Create new Project Protected API endpoint
 @app.route('/api/projects/add/', methods = ['POST'])
+@jwt_required()
 def add_new_project():
      #call add_project() from db.py
     #if data comes back return 201,
@@ -92,6 +93,7 @@ def add_new_project():
 
 # Edit existing Project Protected API endpoint
 @app.route('/api/projects/edit/<project_name>', methods = ['PUT'])
+@jwt_required()
 def edit_project(project_name):
     #Retrieve Project from Mongodb
     #
@@ -99,6 +101,7 @@ def edit_project(project_name):
 
 # Delete existing Project Protected API endpoint
 @app.route('/api/projects/delete/<project_name>', methods = ['DELETE'])
+@jwt_required()
 def delete_project(project_name):
     #Call Delete Project Function in db.py
     #
@@ -109,18 +112,21 @@ def delete_project(project_name):
 
 # Retrieve product Protected API endpoint
 @app.route('/api/products/get/<product_name>', methods=['GET'])
+@jwt_required()
 def get_product_data(product_name):
     data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
     return jsonify(data=data), 200 #200 = Resource retrieved successfully 
 
 # Create new Product Protected API endpoint
 @app.route('/api/products/add/', methods = ['POST'])
+@jwt_required()
 def add_new_product():
     data = [{'name' : "untitled_01", 'products' : []}]
     return jsonify(data=data), 201  #201 = New Resource Created
 
 # Edit existing Product Protected API endpoint
 @app.route('/api/products/edit/<product_name>', methods = ['PUT'])
+@jwt_required()
 def edit_product(product_name):
     #Retrieve Product from Mongodb
     #
@@ -128,6 +134,7 @@ def edit_product(product_name):
 
 # Delete existing Product Protexted API endpoint
 @app.route('/api/products/delete/<product_name>', methods = ['DELETE'])
+@jwt_required()
 def delete_product(product_name):
     #Call Delete Product Function in db.py
     #
@@ -137,18 +144,21 @@ def delete_product(product_name):
 
 # Retrieve Template Protected API endpoint
 @app.route('/api/templates/get/<template_name>', methods=['GET'])
+@jwt_required()
 def get_template_data(template_name):
     data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
     return jsonify(data=data), 200 #200 = Resource retrieved successfully 
 
 # Create new Template Protected API endpoint
 @app.route('/api/templates/add/', methods = ['POST'])
+@jwt_required()
 def add_new_template():
     data = [{'name' : "untitled_01", 'products' : []}]
     return jsonify(data=data), 201  #201 = New Resource Created
 
 # Edit existing Template Protected API endpoint
 @app.route('/api/templates/edit/<template_name>', methods = ['PUT'])
+@jwt_required()
 def edit_template(template_name):
     #Retrieve Product from Mongodb
     #
@@ -156,6 +166,7 @@ def edit_template(template_name):
 
 # Delete existing Template Protexted API endpoint
 @app.route('/api/templates/delete/<template_name>', methods = ['DELETE'])
+@jwt_required()
 def delete_template(template_name):
     #Call Delete Product Function in db.py
     #
