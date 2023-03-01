393e47747ab5c07e89cb93470e46726d54ce2e18
oyrussell <oyrussell@crimson.ua.edu>
2023-02-28 20:12:20 -0600

    Secured API login credentials + more api functions

--- API/app.py
+++ API/app.py
@@ -3,36 +3,32 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
-#import db
+from db import DBClient
 
-load_dotenv()
+load_dotenv() #Init Environment Variables
+app = Flask(__name__) #Flask API Init
 
-app = Flask(__name__)
 # Configure JWT
-
-#Secret Key storing in environment variables (Prevents Brute Forcing Access Tokens to enter database)
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 
+#Test API Endpoint to ensure it is up and running
 @app.route('/api/foo/', methods=['GET'])
 def foo():
     return jsonify(message = "Connection to API Seccessful"), 200
-
-#login System Currently Broken (Working on Fix)
 
 # Login endpoint
 @app.route('/api/login/', methods=['POST'])
 def login():
     username = request.json.get('username', None)
     password = request.json.get('password', None)
-    if username != "admin" or password != "password":
+    if username != os.environ.get('API_USERNAME') or password != os.environ.get('API_PASSWORD'):
         return jsonify(error='Invalid username or password'), 401 #401 Unauthorized Request
-    
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token), 200  #Resource retrieved succefully (Unique JWT for each login session)
 
-'''
-How to call the login API endpoint and store the access_token for future calls to protected endpoints:
+''' ***************   FOR FRONTEND  ***************
+General Structure for how to call the login API endpoint and store the access_token for future calls to protected endpoints:
 
 fetch('/api/login/', {
   method: 'POST',
@@ -62,15 +58,21 @@ fetch('/api/login/', {
 '''
 
 #############################    #Projects API Endpoints     #############################
+# Protected API endpoint
+# @app.route('/api/projects/get/<project_name>', methods=['GET'])
+# @jwt_required()
+# def get_data(project_name):
+#     data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
+#     return jsonify(data=data), 200 #200 = Resource retrieved successfully 
 
 # Protected API endpoint
-@app.route('/api/projects/get/<project_name>', methods=['GET'])
+@app.route('/api/projects/get/', methods=['GET'])
 @jwt_required()
-def get_data(project_name):
+def get_data():
     data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
     return jsonify(data=data), 200 #200 = Resource retrieved successfully 
 
-'''
+''' ***************   FOR FRONTEND  ***************
 How to call a protected endpoint in the frontend:
 
 fetch('/api/projects/get/' + project_name, {
@@ -86,43 +88,38 @@ fetch('/api/projects/get/' + project_name, {
   // handle the error
 });
 '''
-
 # Create new Project Protected API endpoint
 @app.route('/api/projects/add/', methods = ['POST'])
 @jwt_required()
 def add_new_project():
-     #call add_project() from db.py
-    #if data comes back return 201,
-    #else return jsonify(error = 'Failed to Generate New Product'), 400
-    data = [{'name' : "untitled_01", 'products' : []}]
-    return jsonify(data=data), 201  #201 = New Resource Created
+    return jsonify(data=DBClient.add_project()), 201 #Returns a String of the untitled project name but I threw it into a json
 
 # Edit existing Project Protected API endpoint
 @app.route('/api/projects/edit/<project_name>', methods = ['PUT'])
 @jwt_required()
 def edit_project(project_name):
-    #Retrieve Project from Mongodb
-    #
-    return 400  #Unfinished
+    return 400  #Unfinished, still need to figure out how updating works
 
 # Delete existing Project Protected API endpoint
 @app.route('/api/projects/delete/<project_name>', methods = ['DELETE'])
 @jwt_required()
 def delete_project(project_name):
-    #Call Delete Project Function in db.py
-    #
-    return 400 #Unfinished
+    if DBClient.delete_project(project_name):
+        return jsonify(data="Delete Sucessful"), 200 #Seccessful Delete
+    else:
+        return jsonify(data=f"Delete Operation failed on project:{project_name}. 204 Status Code."), 204 #Failed Delete (No Content)
 
 
 #############################   #Products API Endpoints     #############################
-
 # Retrieve product Protected API endpoint
 @app.route('/api/products/get/<product_name>', methods=['GET'])
 @jwt_required()
 def get_product_data(product_name):
-    data = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
-    return jsonify(data=data), 200 #200 = Resource retrieved successfully 
-
+    if DBClient.get_products(product_name):
+        return jsonify(data=DBClient.get_products(product_name)), 200 #Sucessful Product Fetch
+    else:
+        return jsonify(data=f"Error Fetching Product {product_name}. Status Code: 204"), 204 #Failed to Retrieve Data (Doesn't Exist)
+    
 # Create new Product Protected API endpoint
 @app.route('/api/products/add/', methods = ['POST'])
 @jwt_required()
--- API/login_unit_test.py
+++ API/login_unit_test.py
@@ -1,19 +1,21 @@
 try:
     from app import app
     import unittest
+    from dotenv import load_dotenv
+    import os
 
 except Exception as e:
     print("Something does not work\n {}".format(e))
 
 class LoginTest(unittest.TestCase):
-
     def setUp(self):
         self.tester = app.test_client(self)
 
     def test_valid_login(self):
+        load_dotenv() #Init Environment Variables
         data = {
-            'username': 'admin',
-            'password': 'password'
+            'username': os.environ.get('API_USERNAME'),
+            'password': os.environ.get('API_PASSWORD')
         }
         response = self.tester.post('/api/login/', json=data)
         self.assertEqual(response.status_code, 200)
