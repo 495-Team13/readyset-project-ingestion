953a0a98c81cc8384dd76a68b4fb7b028f530a5e
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-11 20:23:30 -0500

    Update EditProject.jsx
    add JSON dot stringify methods to the current project storage and added the label to the stateVars

--- src/EditProject.jsx
+++ src/EditProject.jsx
@@ -14,11 +14,11 @@ export const EditProject = (props) => {
             redirect: "follow"
         };
         if(props.stateVars === "Untitled") {
-            localStorage.setItem("current_project", {"current_project":"new project"});
+            localStorage.setItem("current_project", JSON.Stringify({"current_project":"new project"}));
             console.log("Creating new project");
         } else {
             const str = 'http://ingestion-sandbox.dev.readysetvr.com/api/projects/get/' + props.stateVars;
-            localStorage.setItem("current_project", props.stateVars);
+            localStorage.setItem("current_project", JSON.Stringify({"current_project":props.stateVars});
             fetch(str, requestOptions)
               .then(response => response.json())
               .then(fetchData => {
