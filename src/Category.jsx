4bdc5b77a8f441263a1496c2343fe75c8cb5224d
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-13 17:28:56 -0500

    Update Category.jsx
    added rest of search categories

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,10 +1,11 @@
 import { MainHeader } from "./MainHeader";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Category = (props) => {
 
     const [theme, setTheme] = useState(props.themeState);
     const [data, setData] = useState([]);
+    const [value, setValue] = useState('');
     
     const render = () => {
         var mounted = true;
@@ -76,7 +77,18 @@ export const Category = (props) => {
                     <td><button className="editrecord" onClick={deleteRecord}>Delete Selected</button></td>
                 </tr>
                 <tr>
-                    <td>
+        <div className="background" id={theme}>
+            <table className="projects" id={theme}> 
+                <tbody>
+                    <tr>
+                        <td><table><tbody><tr>
+                            <td><h2 className="projects">Projects</h2></td>
+                            <td><button className="projects" id="add-project" onClick={() => props.onSwitch('EditProject', 'Untitled', theme)}>+</button></td>
+                            <td><input className="projects" value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Search..."></input></td>
+                        </tr></tbody></table></td>
+                    </tr>
+                    <tr>
+                        <td>
                         <div className="search-container"> 
                             <div className="dropdown">
                                     {data.filter(item => {
@@ -94,8 +106,11 @@ export const Category = (props) => {
                                         </div>
                                     ))} 
                             </div> 
-                         </div>    
-                    </td>
+                         </div>
+                         </td>
+                    </tr>
+                </tbody>
+            </table>
                 </tr>
                 <tr>
                     <td> {/* What goes here? */}</td>
