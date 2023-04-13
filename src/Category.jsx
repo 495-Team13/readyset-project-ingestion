9684279842b5b21efa38a3454469e62a7dc69c03
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-13 17:39:24 -0500

    Update Category.jsx
    added <td></td> tags to the search table

--- src/Category.jsx
+++ src/Category.jsx
@@ -77,40 +77,41 @@ export const Category = (props) => {
                     <td><button className="editrecord" onClick={deleteRecord}>Delete Selected</button></td>
                 </tr>
                 <tr>
-        <div className="background" id={theme}>
-            <table className="projects" id={theme}> 
-                <tbody>
-                    <tr>
-                        <td><table><tbody><tr>
-                            <td><h2 className="projects">Projects</h2></td>
-                            <td><button className="projects" id="add-project" onClick={() => props.onSwitch('EditProject', 'Untitled', theme)}>+</button></td>
-                            <td><input className="projects" value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Search..."></input></td>
-                        </tr></tbody></table></td>
-                    </tr>
-                    <tr>
-                        <td>
-                        <div className="search-container"> 
-                            <div className="dropdown">
-                                    {data.filter(item => {
-                                        const searchTerm = value.toLowerCase();
-                                        const name = item.name.toLowerCase();
+                    <td>
+                        <table className="projects" id={theme}> 
+                            <tbody>
+                                <tr>
+                                    <td><table><tbody><tr>
+                                        <td><h2 className="projects">Projects</h2></td>
+                                        <td><button className="projects" id="add-project" onClick={() => props.onSwitch('EditProject', 'Untitled', theme)}>+</button></td>
+                                        <td><input className="projects" value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Search..."></input></td>
+                                    </tr></tbody></table></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div className="search-container"> 
+                                            <div className="dropdown">
+                                                {data.filter(item => {
+                                                    const searchTerm = value.toLowerCase();
+                                                    const name = item.name.toLowerCase();
 
-                                        return (searchTerm && name.startsWith(searchTerm)) || value === '';
-                                    }).map((item) => (
-                                        <div className="dropdown-row"  key={item.name}>
-                                            <table><tbody><tr>
-                                                <td><p>{item.name}</p></td>
-                                                <td><button className="projects" id="green" onClick={()=>onSearch(item.name)}>Edit</button></td>
-                                                <td><button className="projects" id="red" onClick={()=>deleteButton(item)}>Delete</button></td>
-                                            </tr></tbody></table>
+                                                    return (searchTerm && name.startsWith(searchTerm)) || value === '';
+                                                }).map((item) => (
+                                                    <div className="dropdown-row"  key={item.name}>
+                                                        <table><tbody><tr>
+                                                            <td><p>{item.name}</p></td>
+                                                            <td><button className="projects" id="green" onClick={()=>onSearch(item.name)}>Edit</button></td>
+                                                            <td><button className="projects" id="red" onClick={()=>deleteButton(item)}>Delete</button></td>
+                                                        </tr></tbody></table>
+                                                    </div>
+                                                ))} 
+                                            </div> 
                                         </div>
-                                    ))} 
-                            </div> 
-                         </div>
-                         </td>
-                    </tr>
-                </tbody>
-            </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
                 </tr>
                 <tr>
                     <td> {/* What goes here? */}</td>
