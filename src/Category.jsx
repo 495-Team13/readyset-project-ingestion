474c762135b4ae945931a0b1d9d14b0cf2393f5a
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-13 17:50:24 -0500

    Update Category.jsx
    changed to say categories

--- src/Category.jsx
+++ src/Category.jsx
@@ -81,7 +81,7 @@ export const Category = (props) => {
                             <tbody>
                                 <tr>
                                     <td><table><tbody><tr>
-                                        <td><h2 className="projects">Projects</h2></td>
+                                        <td><h2 className="projects">Categories</h2></td>
                                         <td><button className="projects" id="add-project" onClick={() => props.onSwitch('EditProject', 'Untitled', theme)}>+</button></td>
                                         <td><input className="projects" value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Search..."></input></td>
                                     </tr></tbody></table></td>
