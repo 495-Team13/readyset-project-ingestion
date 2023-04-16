1d0a272664c57a114f674a74848d2f4b5b4b6332
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-15 22:27:43 -0500

    Create EditUser.jsx
    created file

---
+++ src/EditUser.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { MainHeader } from "./MainHeader";
+
+export const EditUser = (props) => {
+  
+  const [theme, setTheme] = useState(props.themeState);
+  const [data, setData] = useState([]);
+  
+  const render = () => {
+        var mounted = true;
+        const obj = JSON.parse(localStorage.getItem("access_token"));
+        const token = "Bearer " + obj.access_token;
+        
+        var requestOptions = {
+            method: "GET",
+            headers: {"Authorization":token},
+            redirect: "follow"   
+        };
+                
+        fetch("https://ingestion-sandbox.dev.readysetvr.com/api/users/get/" + props.stateVars, requestOptions)
+                .then(response => response.json())
+                .then(fetchData => {
+                    if(mounted) {
+                        setData(fetchData);   
+                        console.log(data);
+                    }
+                });
+        return () => mounted = false;
+  }
+  
+  useEffect(() => {
+    render();
+  }, [])
+    
+  const changeTheme =(newTheme) => {
+    setTheme(newTheme);
+  }
+  
+  return (
+    <div className="background" id={theme}>
+      <MainHeader current_theme={theme} switchTheme={changeTheme} onSwitch={(stateName, stateVars, theme) => props.onSwitch(stateName, stateVars, theme)} />
+    </div>
+  )
+  
+}
