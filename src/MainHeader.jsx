7b86029a69485f7d7e597e5716876010af610455
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-15 19:11:09 -0500

    Update MainHeader.jsx
    changed label to "categories" from "templates"

--- src/MainHeader.jsx
+++ src/MainHeader.jsx
@@ -11,7 +11,7 @@ export const MainHeader = (props) => {
                     <tr>
                         <td><h1>ReadySet</h1></td>
                         <td><button id="header-button" onClick={() => props.onSwitch('Projects', '', props.current_theme)}>Projects</button></td>
-                        <td><button id="header-button" onClick={() => props.onSwitch('Category', '', props.current_theme)}>Templates</button></td>
+                        <td><button id="header-button" onClick={() => props.onSwitch('Category', '', props.current_theme)}>Categories</button></td>
                         <td><button id="header-button" onClick={toggleTheme}>{props.current_theme}</button></td>
                         <td><button id="header-button" onClick={() => props.onSwitch('Login','',props.current_theme)}>Log out</button></td>
                     </tr>
