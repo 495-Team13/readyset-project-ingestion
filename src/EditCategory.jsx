d319b29a912b135f4037063cac3ea7b1fa0d7237
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-15 20:54:21 -0500

    Update EditCategory.jsx
    added value, and the display for the templates

--- src/EditCategory.jsx
+++ src/EditCategory.jsx
@@ -5,6 +5,7 @@ export const EditCategory = (props) => {
 
     const [theme, setTheme] = useState(props.themeState);
     const [data, setData] = useState([]);
+    const [value, setValue] = useState('');
     
     const render = () => {
         var mounted = true;
@@ -36,8 +37,41 @@ export const EditCategory = (props) => {
     }
 
     return(
-        <div className="background" id={theme}>
+        <div id={theme} class="background">
             <MainHeader current_theme={theme} switchTheme={changeTheme} onSwitch={(stateName, stateVars, theme) => props.onSwitch(stateName, stateVars, theme)} />
+                        <table className="projects" id={theme}> 
+                            <tbody>
+                                <tr>
+                                    <td><table><tbody><tr>
+                                        <td><h2 className="projects">Categories</h2></td>
+                                        <td><button className="projects" id="add-project" onClick={() => props.onSwitch('EditProject', 'Untitled', theme)}>+</button></td>
+                                        <td><input className="projects" value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Search..."></input></td>
+                                    </tr></tbody></table></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div className="search-container"> 
+                                            <div className="dropdown">
+                                                {data.filter(item => {
+                                                    const searchTerm = value.toLowerCase();
+                                                    const name = item.name.toLowerCase();
+
+                                                    return (searchTerm && name.startsWith(searchTerm)) || value === '';
+                                                }).map((item) => (
+                                                    <div className="dropdown-row"  key={item.name}>
+                                                        <table><tbody><tr>
+                                                            <td><p>{item.name}</p></td>
+                                                            <td><button className="projects" id="green" onClick={()=>onSearch(item.name)}>Edit</button></td>
+                                                            <td><button className="projects" id="red" onClick={()=>deleteCategory(item.name)}>Delete</button></td>
+                                                        </tr></tbody></table>
+                                                    </div>
+                                                ))} 
+                                            </div> 
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
         </div>
     )
 
