fc4125edf554b17983df9c1df22470f110c0107d
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-21 19:47:23 -0500

    Update EditTemplate.jsx
    ';decaf

--- src/EditTemplate.jsx
+++ src/EditTemplate.jsx
@@ -123,7 +123,6 @@ export const EditTemplate = (props) => {
             editTemplate();
             render();
         }
-        updateCategory();
         props.onSwitch("Category", '', theme);
     }
     
