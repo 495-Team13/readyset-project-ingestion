17491f56690cbb6350eb6bb9309c7e9699ea9309
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-23 18:47:56 -0500

    Create ModelViewer.jsx
    create model viewer component

---
+++ src/ModelViewer.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "GLTFLoader.js";
+
+function ModelViewer(props) {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const mount = mountRef.current;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      mount.clientWidth / mount.clientHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(mount.clientWidth, mount.clientHeight);
+    mount.appendChild(renderer.domElement);
+
+    const loader = new GLTFLoader();
+    loader.load(props.modelPath, (gltf) => {
+      scene.add(gltf.scene);
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+    animate();
+
+    return () => {
+      mount.removeChild(renderer.domElement);
+    };
+  }, [props.modelPath]);
+
+  return <div ref={mountRef} />;
+}
+
+export default ModelViewer;
