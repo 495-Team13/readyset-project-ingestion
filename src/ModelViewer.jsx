e9365a9007505e35a1f7023d244c8faf33ae04cf
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-23 18:51:41 -0500

    Update ModelViewer.jsx
    added ./

--- src/ModelViewer.jsx
+++ src/ModelViewer.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "GLTFLoader.js";
+import { GLTFLoader } from "./GLTFLoader.js";
 
 function ModelViewer(props) {
   const mountRef = useRef(null);
