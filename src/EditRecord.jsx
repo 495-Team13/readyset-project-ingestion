df647fcf1047205d6ecaa29b0a65bfcd1eda7d5a
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-24 15:06:19 -0500

    Update EditRecord.jsx
    added default "checked" when there is drc

--- src/EditRecord.jsx
+++ src/EditRecord.jsx
@@ -27,7 +27,7 @@ export const EditRecord = (props) => {
     const [type, set_type] = useState('');
     const [workflow, set_workflow] = useState('');
         /* Inferred Hooks */
-    const [drc, set_drc] = useState('No');
+    const [drc, set_drc] = useState('');
     
     useEffect(() => {
         var mounted = true;
@@ -59,7 +59,7 @@ export const EditRecord = (props) => {
                         setDepth(fetchData.depth);
                         setDrcUpc(fetchData.drc_upc);
                         if(fetchData.drc_upc !== '') {
-                            set_drc('Yes');
+                            set_drc('checked');
                         }
                         setHeight(fetchData.height);
                         setName(fetchData.name);
@@ -193,7 +193,7 @@ export const EditRecord = (props) => {
                 </tr>
                 <tr>
                         <td><input className="editrecord" type="text" placeHolder={upc} onChange={(e) => setUpc(e.target.value)}></input></td>
-                        <td><input className="editrecord" type="checkbox" placeHolder={drc} ></input></td>
+                        <td><input className="editrecord" type="checkbox" {drc} ></input></td>
                         <td><input className="editrecord" type="text" placeHolder={drc_upc} onChange={(e) => setDrcUpc(e.target.value)}></input></td> 
                         <td><input className="editrecord" type="text" placeHolder={gltf} onChange={(e) => set_gltf(e.target.value)}></input></td>
                 </tr>
