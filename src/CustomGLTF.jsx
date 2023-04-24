9996d961a133e5b02d0e50ea9c4872c6d79aa701
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-23 19:48:15 -0500

    Create CustomGLTF.jsx
    create file and hopefully box

---
+++ src/CustomGLTF.jsx
@@ -0,0 +1,17 @@
+import React, {useRef, useState} from 'react'
+import Canvas from '@react-three/fiber'
+import {OrbitControls, useGLTF} from '@react-three/drei'
+
+export const CustomGLTF = (props) => {
+  const ref = useRef();
+  return( 
+    <div>
+      <Canvas>
+        <mesh ref={ref}>
+          <boxGeometry attach='geometry' args=[2,2,2] />
+        </mesh>
+      </Canvas>
+    </div>
+  );
+  
+}
