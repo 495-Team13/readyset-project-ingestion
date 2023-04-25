afb44f0297cac28a393715d0f0294d2a5a20f2f7
Josh <jclacount@crimson.ua.edu>
2023-04-25 00:40:39 +0000

    styling auth.py

--- API/auth.py
+++ API/auth.py
@@ -1,7 +1,26 @@
+"""Functions for user authentication."""
+
 import bcrypt
 
 def hash_password(password):
+    """Salts and hashes password.
+
+    Args:
+      password: Password to hash.
+
+    Returns:
+      Hashed password.
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 def verify_password(guessed_password, hashed_password):
+    """Verifies if a plain-text password matches a hashed password.
+
+    Args:
+      guessed_password: Plain-text password to check.
+      hashed_password: Hashed password that's checked against.
+
+    Returns:
+      True if match otherwise False
+    """
     return bcrypt.checkpw(guessed_password.encode('utf-8'), hashed_password)
