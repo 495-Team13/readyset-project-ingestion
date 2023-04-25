b36068ff72b89aee6b39b0ab08f5e55ba7dfbe52
jwlevendoski <72052801+jwlevendoski@users.noreply.github.com>
2023-04-24 19:12:22 -0500

    Update EditRecord.jsx
    print to make sure checkbox updating is passing

--- src/EditRecord.jsx
+++ src/EditRecord.jsx
@@ -194,7 +194,11 @@ export const EditRecord = (props) => {
                 </tr>
                 <tr>
                         <td><input className="editrecord" type="text" placeHolder={upc} onChange={(e) => setUpc(e.target.value)}></input></td>
-                        <td><input id="drc_checkbox" type="checkbox" onChange={(e)=> set_drc(e.target.value)} ></input></td>
+                        <td><input id="drc_checkbox" type="checkbox" onChange={(e)=> 
+                        {
+                            set_drc(e.target.value);
+                            console.log(drc);
+                        } ></input></td>
                         <td><input className="editrecord" type="text" placeHolder={drc_upc} onChange={(e) => setDrcUpc(e.target.value)}></input></td> 
                         <td><input className="editrecord" type="text" placeHolder={gltf} onChange={(e) => set_gltf(e.target.value)}></input></td>
                 </tr>
