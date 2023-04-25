f4a4c2bb0943b5e49fd562c58a744e9fe94c0920
Josh <jclacount@crimson.ua.edu>
2023-04-25 00:36:47 +0000

    styling export.py

--- API/export.py
+++ API/export.py
@@ -1,33 +1,58 @@
+"""Functions for exporting documents to CSV.
+
+Export function names should be formatted as 'export_<document type>'.
+This is so that export functions can be retrieved via getattr().
+"""
+
 import pandas as pd
 import CRUD
 
 def product_convert_nested(product):
-    product['count'] = '' if product['count']['num'] < 0 else str(product['count']['num'])+product['count']['unit']
-    product['amount'] = str(product['amount']['measurement'])+product['amount']['unit'] 
+    """Converted count and amount fields to single value.
+
+    Args:
+      product: Original product document.
+    """
+    product['count'] = ('' if product['count']['num'] < 0 else
+                        f"{product['count']['num']}{product['count']['unit']}")
+    product['amount'] = (f"{product['amount']['measurement']}" +
+                         {product['amount']['unit']})
 
 def export_project(project):
-    query = {'upc': {'$in': project['products']}}
-    products = CRUD.client.get_products(query)
+    """Export project to CSV.
 
+    Args:
+      project: Project to export.
+
+    Returns:
+      CSV as a string.
+    """
+    products = CRUD.client.get_products({'upc': {'$in': project['products']}})
     drc_upc = []
     for product in products:
         product_convert_nested(product)
         if product['drc_upc'] != '':
             drc_upc.append(product['drc_upc'])
 
-    query = {'upc': {'$in': drc_upc}}
-    drc = CRUD.client.get_products(query)
-
+    drc = CRUD.client.get_products({'upc': {'$in': drc_upc}})
     for product in drc:
         product_convert_nested(product)
-
     products += drc
 
     df = pd.DataFrame(products)
     return df.to_csv(index=False)
 
 def export_category(category):
-    query = {'name': {'$in': category['templates']}}
-    templates = CRUD.client.get_templates(query)
+    """Export category to CSV.
+
+    Args:
+      category: Category to export.
+
+    Returns:
+      CSV as a string.
+    """
+    templates = CRUD.client.get_templates(
+        {'name': {'$in': category['templates']}}
+    )
     df = pd.DataFrame(templates)
     return df.to_csv(index=False)
